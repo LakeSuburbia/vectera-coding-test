2e91c30e9f042dee62a8d7aca4b42021ba022ad2
sander-iqgeo <sander.mangelschots@iqgeo.com>
2026-07-06 13:00:41 +0200

    Front-end: Create meeting-list component

---
+++ frontend/src/app/meetings/meeting-list/meeting-list.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Meeting, CreateMeetingPayload } from '../../core/models/meeting.model';
+import { MeetingService } from '../../core/services/meeting.service';
+
+@Component({
+  selector: 'app-meeting-list',
+  templateUrl: './meeting-list.component.html',
+})
+export class MeetingListComponent implements OnInit {
+  meetings: Meeting[] = [];
+  loading = false;
+  error: string | null = null;
+
+  page = 1;
+  hasNext = false;
+  hasPrevious = false;
+
+  newMeeting: CreateMeetingPayload = { title: '', started_at: '' };
+  creating = false;
+  createError: string | null = null;
+
+  constructor(private meetingService: MeetingService) {}
+
+  ngOnInit(): void {
+    this.loadMeetings(1);
+  }
+
+  loadMeetings(page: number): void {
+    this.loading = true;
+    this.error = null;
+    this.meetingService.getMeetings(page).subscribe({
+      next: (response) => {
+        this.meetings = response.results;
+        this.hasNext = !!response.next;
+        this.hasPrevious = !!response.previous;
+        this.page = page;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load meetings.';
+        this.loading = false;
+      },
+    });
+  }
+
+  nextPage(): void {
+    if (this.hasNext) this.loadMeetings(this.page + 1);
+  }
+
+  previousPage(): void {
+    if (this.hasPrevious) this.loadMeetings(this.page - 1);
+  }
+
+  summaryBadge(meeting: Meeting): string {
+    return meeting.latest_summary?.status ?? 'none';
+  }
+
+  createMeeting(): void {
+    if (!this.newMeeting.title || !this.newMeeting.started_at) return;
+    this.creating = true;
+    this.createError = null;
+    this.meetingService.createMeeting(this.newMeeting).subscribe({
+      next: () => {
+        this.creating = false;
+        this.newMeeting = { title: '', started_at: '' };
+        this.loadMeetings(1);
+      },
+      error: () => {
+        this.creating = false;
+        this.createError = 'Failed to create meeting. Check the title and start date.';
+      },
+    });
+  }
+}
